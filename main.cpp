1b5a324328ddfd2e6f59c49a06858caae73a53f0
Nabeedjamshed <nabeedjamshedali800@gmail.com>
2024-09-29 22:11:14 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int fabonacci(int n, vector <int> &dp){
+    if (n<=1)
+    {
+        return n;
+    } if (dp[n] != -1) 
+    {
+        return dp[n];
+    } else {
+        return dp[n] = fabonacci(n-2, dp) + fabonacci(n-1, dp);
+    }
+}
+int main() {
+    int n;
+    cout<<"Enter num: ";
+    cin>>n;
+    vector <int> dp(n+1,-1);
+    vector <int> l;
+    for (int i = 0; i <= n; i++)
+    {
+        l.push_back(fabonacci(i,dp));
+    }
+    
+    for (auto a:l)
+    {
+        cout<<a<<" ";
+    }
+    return 0;
+}
